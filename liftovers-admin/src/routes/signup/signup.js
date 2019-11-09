3b2865329409a8950175e30b233103a5a1216059
ngryan3 <55714901+ngryan3@users.noreply.github.com>
2019-11-08 22:19:24 -0500

    Update signup.js

--- liftovers-admin/src/routes/signup/signup.js
+++ liftovers-admin/src/routes/signup/signup.js
@@ -42,10 +42,14 @@ class Signup extends React.Component {
       e.preventDefault();
       if (this.validateForm()) {
           let fields = {};
-          fields["username"] = "";
-          fields["emailid"] = "";
-          fields["mobileno"] = "";
+          fields["firstname"] = "";
+          fields["lastname"] = "";
+          fields["phonenumber"] = "";
+          fields["address"] = "";
+          fields["postalcode"] = "";
+          fields["email"] = "";
           fields["password"] = "";
+          fields["passconfirmed"] = "";
           this.setState({fields:fields});
           alert("Form submitted");
       }
@@ -53,60 +57,97 @@ class Signup extends React.Component {
     }
 
     validateForm() {
+        let fields = this.state.fields;
+        let errors = {};
+        let formIsValid = true;
 
-      let fields = this.state.fields;
-      let errors = {};
-      let formIsValid = true;
-
-      if (!fields["username"]) {
-        formIsValid = false;
-        errors["username"] = "*Please enter your username.";
-      }
-
-      if (typeof fields["username"] !== "undefined") {
-        if (!fields["username"].match(/^[a-zA-Z ]*$/)) {
-          formIsValid = false;
-          errors["username"] = "*Please enter alphabet characters only.";
+        if (!fields["firstname"]) {
+            formIsValid = false;
+            errors["firstname"] = "*Please enter your first name.";
         }
-      }
 
-      if (!fields["emailid"]) {
-        formIsValid = false;
-        errors["emailid"] = "*Please enter your email-ID.";
-      }
-
-      if (typeof fields["emailid"] !== "undefined") {
-        //regular expression for email validation
-        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-        if (!pattern.test(fields["emailid"])) {
-          formIsValid = false;
-          errors["emailid"] = "*Please enter valid email-ID.";
+        if (!fields["lastname"]) {
+            formIsValid = false;
+            errors["lastname"] = "*Please enter your last name.";
         }
-      }
 
-      if (!fields["mobileno"]) {
-        formIsValid = false;
-        errors["mobileno"] = "*Please enter your mobile no.";
-      }
-
-      if (typeof fields["mobileno"] !== "undefined") {
-        if (!fields["mobileno"].match(/^[0-9]{10}$/)) {
-          formIsValid = false;
-          errors["mobileno"] = "*Please enter valid mobile no.";
+        if (typeof fields["firstname"] !== "undefined" || typeof fields["lastname"] !== "undefined") {
+            if (!fields["firstname"].match(/^[a-zA-Z]*$/)) {
+              formIsValid = false;
+              errors["firstname"] = "*Please enter alphabet characters only.";
+            }
+              if (!fields["lastname"].match(/^[a-zA-Z]*$/)) {
+                  formIsValid = false;
+                  errors["lastname"] = "*Please enter alphabet characters only.";
+              }
         }
-      }
 
-      if (!fields["password"]) {
-        formIsValid = false;
-        errors["password"] = "*Please enter your password.";
-      }
-
-      if (typeof fields["password"] !== "undefined") {
-        if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
-          formIsValid = false;
-          errors["password"] = "*Please enter secure and strong password.";
+        if (!fields["phonenumber"]) {
+            formIsValid = false;
+            errors["phonenumber"] = "*Please enter your phone number.";
         }
-      }
+
+        if (typeof fields["phonenumber"] !== "undefined") {
+            if (!fields["phonenumber"].match(/^[0-9]{10}$/)) {
+                formIsValid = false;
+                errors["phonenumber"] = "*Please enter a valid mobile no.";
+            }
+        }
+
+        if (!fields["address"]) {
+            formIsValid = false;
+            errors["address"] = "*Please enter your address.";
+        }
+
+        if (typeof fields["address"] !== "undefined") {
+            if (!fields["address"].match(/^[a-zA-Z0-9]*$/)) {
+                formIsValid = false;
+                errors["address"] = "*Please enter alphanumeric characters only.";
+            }
+        }
+
+        if (!fields["postalcode"]) {
+            formIsValid = false;
+            errors["postalcode"] = "*Please enter your postal code.";
+        }
+
+        if (typeof fields["postalcode"] !== "undefined") {
+            if (!fields["postalcode"].match(/^[a-zA-Z0-9]{6}$/)) {
+                formIsValid = false;
+                errors["postalcode"] = "*Please enter a valid postal code.";
+            }
+        }
+
+        if (!fields["email"]) {
+            formIsValid = false;
+            errors["email"] = "*Please enter your email.";
+        }
+
+        if (typeof fields["email"] !== "undefined") {
+            //regular expression for email validation
+            let pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+            if (!pattern.test(fields["email"])) {
+              formIsValid = false;
+              errors["email"] = "*Please enter valid email.";
+            }
+        }
+
+        if (!fields["password"] || !fields["passconfirmed"]) {
+            formIsValid = false;
+            errors["password"] = "*Please enter your password.";
+        }
+
+        if (typeof fields["password"] !== "undefined") {
+            if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+              formIsValid = false;
+              errors["password"] = "*Please enter secure and strong password.";
+            }
+        }
+
+        if (fields["password"] !== fields["passconfirmed"]){
+            formIsValid = false;
+            errors["passconfirmed"] = "*Not matching passwords"
+        }
 
       this.setState({
         errors: errors
@@ -153,31 +194,33 @@ class Signup extends React.Component {
                     <Form.Group>
                         <label style={{marginLeft: '10px'}}>
                         First Name:
-                            <input style={{marginLeft: '30px'}}  size="lg" type="text" name="username" value={this.state.fields.username} onChange={this.handleChange} />
+                            <input style={{marginLeft: '30px'}}  size="lg" type="text" name="firstname" value={this.state.fields.username} onChange={this.handleChange} />
                         </label>
-                        <div className="errorMsg"  style={{ color: 'red' }}>{this.state.errors.username}</div>
+                        <div className="errorMsg"  style={{ color: 'red' }}>{this.state.errors.firstname}</div>
                         <br />
                     </Form.Group>
                     <br />
                     <Form.Group>
                         <label style={{marginLeft: '10px'}}>
                         Last Name:
-                        <input style={{marginLeft: '31px'}}  type="text" name="lname" />
+                            <input style={{marginLeft: '30px'}}  size="lg" type="text" name="lastname" value={this.state.fields.username} onChange={this.handleChange} />
                         </label>
+                        <div className="errorMsg"  style={{ color: 'red' }}>{this.state.errors.lastname}</div>
                     </Form.Group>
                     <br />
                     <Form.Group row>
                         <label  style={{marginLeft: '10px'}}>
                         Phone Number:
-                        <input style={{marginLeft: '1px'}} type="text" name="mobileno" value={this.state.fields.mobileno} onChange={this.handleChange} />
+                        <input style={{marginLeft: '1px'}} type="text" name="phonenumber" value={this.state.fields.mobileno} onChange={this.handleChange} />
                         </label>
-                        <div className="errorMsg" style={{ color: 'red' }}>{this.state.errors.mobileno}</div>
+                        <div className="errorMsg" style={{ color: 'red' }}>{this.state.errors.phonenumber}</div>
                     </Form.Group>
                     <br />
                     <Form.Group row>
                         <label  style={{marginLeft: '10px'}}>
                         Main Address:
-                        <input   style={{marginLeft: '10px'}}type="text" name="addr" />
+                            <input style={{marginLeft: '30px'}}  size="lg" type="text" name="address" value={this.state.fields.username} onChange={this.handleChange} />
+                            <div className="errorMsg" style={{ color: 'red' }}>{this.state.errors.address}</div>
                         </label> <br />
                         <Form.Text className="text-muted">
                         We'll never share your address with anyone else.
@@ -188,7 +231,8 @@ class Signup extends React.Component {
                         <label  style={{marginLeft: '10px'}}>
                         Main Address Postal Code:
                         <br />
-                        <input  style={{marginLeft: '120px'}} type="text" name="pcode" />
+                            <input style={{marginLeft: '30px'}}  size="lg" type="text" name="postalcode" value={this.state.fields.username} onChange={this.handleChange} />
+                            <div className="errorMsg" style={{ color: 'red' }}>{this.state.errors.postalcode}</div>
                         </label> <br />
                         <Form.Text className="text-muted">
                         We'll never share your Postal code with anyone else.
@@ -198,9 +242,9 @@ class Signup extends React.Component {
                     <Form.Group row>
                         <label  style={{marginLeft: '10px'}}>
                         Email Address:
-                        <input  style={{marginLeft: '8px'}} type="text" name="emailid" value={this.state.fields.emailid} onChange={this.handleChange}/>
+                        <input  style={{marginLeft: '8px'}} type="text" name="email" value={this.state.fields.email} onChange={this.handleChange}/>
                         </label>
-                        <div className="errorMsg" style={{ color: 'red' }}>{this.state.errors.emailid}</div>
+                        <div className="errorMsg" style={{ color: 'red' }}>{this.state.errors.email}</div>
                          <br />
                         <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
@@ -211,7 +255,7 @@ class Signup extends React.Component {
                     <Form.Group row>
                         <label  style={{marginLeft: '10px'}}>
                         Password:
-                        <input  style={{marginLeft: '40px'}} type="text" name="password"  value={this.state.fields.password} onChange={this.handleChange}  />
+                        <input  style={{marginLeft: '40px'}} type="password" name="password"  value={this.state.fields.password} onChange={this.handleChange}  />
                         </label>
                         <div className="errorMsg" style={{ color: 'red' }}>{this.state.errors.password}</div>
                     </Form.Group>
@@ -223,7 +267,8 @@ class Signup extends React.Component {
                     <Form.Group row>
                         <label  style={{marginLeft: '10px'}}>
                         Re-enter Password:
-                        <input  style={{marginLeft: '10px'}} type="text" name="passConf" />
+                            <input style={{marginLeft: '30px'}}  size="lg" type="password" name="passconfirmed" value={this.state.fields.username} onChange={this.handleChange} />
+                            <div className="errorMsg" style={{ color: 'red' }}>{this.state.errors.passconfirmed}</div>
                         </label>
                     </Form.Group>
                     <br />
