6059c41244166b99667642d422a4f1de9fa3b44a
Faye (Jingfei) Tan <jingfei.tan@mail.utoronto.ca>
2019-11-12 23:10:02 -0500

    Added get and post urls for users

--- liftovers-api/app/routes/index.js
+++ liftovers-api/app/routes/index.js
@@ -1,12 +1,16 @@
 module.exports = function(app) {
   var volunteer = require("../controllers/volunteer.js");
+  var user = require("../controllers/user.js");
   var lift = require("../controllers/lifts.js");
 
   // Create a new Note
   app.post("/volunteer", volunteer.create);
+  app.post("/user", user.create);
   app.post("/volunteers/donors", volunteer.getDistance);
   app.post("/volunteers/foodbank", volunteer.getDistanceBanks);
   app.post("/lift/request", lift.requestLift);
   app.post("/volunteer/availability", volunteer.acceptText);
+
   app.get("/volunteer", volunteer.findAll);
+  app.get("/user", user.findAll);
 };
