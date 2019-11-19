6377b05df2b772a4b37043636a2a78206a4cb70e
Faye (Jingfei) Tan <jingfei.tan@mail.utoronto.ca>
2019-11-19 14:21:35 -0500

    Added models for Donor and Provider, as well as the GET/POST urls.

---
+++ liftovers-api/app/controllers/provider.js
@@ -0,0 +1,42 @@
+var Provider = require("../models/provider.js");
+
+// Create and Save a new Note
+exports.create = function(req, res) {
+  // Create and Save a new Note
+  if (!req.body.contactName) {
+    return res.status(400).send({ message: "Contact Name can not be empty" });
+  }
+
+  var provider = new Provider({
+    contactName: req.body.contactName,
+    contactSurname: req.body.contactSurname,
+    contactEmail: req.body.contactEmail,
+    contactPhone: req.body.contactPhone,
+    methodOfCommunication: req.body.methodOfCommunication,
+    location: req.body.location,
+    hours: req.body.hours,
+    acceptedFoods: req.body.acceptedFoods,
+    unacceptableFoods: req.body.unacceptableFoods,
+  });
+
+  provider.save(function(err, data) {
+    if (err) {
+      console.log(err);
+      res
+        .status(500)
+        .send({ message: "Some error occurred while creating the Provider." });
+    } else {
+      res.send(data);
+    }
+  });
+};
+
+exports.findAll = function(req, res) {
+  // Retrieve and return all notes from the database.
+  let { page = 1, limit = 100 } = req.query;
+  Provider.paginate({}, { page, limit }).then(providers => {
+    if (!providers)
+      return res.status(404).send({ message: "No Providers found." });
+    return res.status(200).send(providers);
+  });
+};
