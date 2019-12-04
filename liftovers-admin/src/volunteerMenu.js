ba6fe06c599f9a52b4810346d4c0bfed946f3702
tomisore <omisoretosin@yahoo.com>
2019-12-04 00:06:22 -0500

    Add files via upload

---
+++ liftovers-admin/src/volunteerMenu.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    name: "My Profile",
+    icon: "icon-user",
+    navlink: "/profile"
+  },
+  {
+    name: "Dashboard",
+    icon: "icon-gauge",
+    navlink: "/dashboard"
+  },
+  {
+    name: "Lifts",
+    icon: "icon-list",
+    navlink: "/lifts"
+  },
+  {
+    name: "Volunteers",
+    icon: "icon-users",
+    navlink: "/volunteers"
+  },
+];
+
