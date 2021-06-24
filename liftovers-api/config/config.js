6d9df6b7a0a544b5eb78e14b1308cfd2063e5ca3
ngryan3 <55714901+ngryan3@users.noreply.github.com>
2021-06-24 19:15:08 -0400

    Delete liftovers-api directory

--- liftovers-api/config/config.js
+++
@@ -1,8 +0,0 @@
-// this connects to the liftovers database, leave this as is
-// the format of this url is "mongodb://[username]:[password]@[host]:[port]/[database]"
-// If u guys are interested in directly accessing the database, download mongodb compass and go to connect
-// Fill in host, port, username and password and connect & you're good to go :)
-module.exports = {
-    url: 'mongodb://heroku_c0q92w65:74dc1vjdtf84m8d1n4jkfslmku@ds231229.mlab.com:31229/heroku_c0q92w65'
-}
-
